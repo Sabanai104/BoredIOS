3930078d72e4340f66099b279b5ab7b4b302f839
Gabriel Sabanai <gabrielsabanaitrindade@gmail.com>
2023-10-21 14:17:38 +0900

    Initial Commit

---
+++ Bored/BoredApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoredApp.swift
+//  Bored
+//
+//  Created by Gabriel Sabanai Trindade on 21/10/23.
+//
+
+import SwiftUI
+
+@main
+struct BoredApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
